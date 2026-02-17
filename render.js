912061e3a60518b13d6ffd68a8582d42d1175831
Dweet Parikh <dweet.parikh16@gmail.com>
2026-02-17 17:04:34 +0530

    added more functionality

--- render.js
+++ render.js
@@ -1,12 +1,33 @@
+const { createElement } = require("react");
+
 let todos = [];
 
 function addTodo() {
     todos.push({
-        title:document.querySelector("input").value;
+        title:document.querySelector("input").value
     })
     render();
 }
 
 function render() {
-    
+    document.querySelector("#todolist").innerHTML = "";
+    for(let i = 0; i < todos.length; i++) {
+        const todo = todos[i];
+
+        const divEl = createElement("div");
+        const todoEl = createElement("h2");
+        const deleteEl = createElement("button");
+
+        todoEl.innerHTML = todo.title;
+
+        deleteEl.onclick = function() {
+            todos.splice(i, 1);
+            render ();
+        };
+
+        divEl.appendChild(todoEl);
+        divEl.appendChild(deleteEl);
+
+        document.querySelector("#todolist").appendChild(divEl);
+    }
 }
