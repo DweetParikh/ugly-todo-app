6f1e41d73ba9e3ed69eb23195d8e5a477101061f
Dweet Parikh <dweet.parikh16@gmail.com>
2026-02-17 18:37:19 +0530

    added more functionality

--- render.js
+++ render.js
@@ -1,31 +1,49 @@
-const { createElement } = require("react");
-
 let todos = [];
 
 function addTodo() {
     todos.push({
         title:document.querySelector("input").value
     })
-    render();
+    render ();
+}
+
+function deleteFirstTodo() {
+    todos.shift();
+    render ();
+}
+
+function deleteLastTodo() {
+    todos.pop();
+    render ();
 }
 
 function render() {
     document.querySelector("#todolist").innerHTML = "";
-    for(let i = 0; i < todos.length; i++) {
+    for(let i = 1; i < todos.length; i++) {
         const todo = todos[i];
 
-        const divEl = createElement("div");
-        const todoEl = createElement("h2");
-        const deleteEl = createElement("button");
+        const divEl = document.createElement("div");
+        divEl.setAttribute("id", 'todo-' + i);
+        divEl.style.display = "flex";
+        divEl.style.alignItems = "center";
+        divEl.style.gap = "10px";
+        divEl.style.margin = "5px";
+
+        const headingEl = document.createElement("h2");
+        headingEl.textContent = i + '. ' + todo.title;
+
+        const todoEl = document.createElement("h2");
+        const deleteEl = document.createElement("button");
 
         todoEl.innerHTML = todo.title;
+        deleteEl.innerHTML = "Delete";
 
         deleteEl.onclick = function() {
             todos.splice(i, 1);
             render ();
         };
 
-        divEl.appendChild(todoEl);
+        divEl.appendChild(headingEl);
         divEl.appendChild(deleteEl);
 
         document.querySelector("#todolist").appendChild(divEl);
